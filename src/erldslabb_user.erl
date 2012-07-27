b294f58e7284cbc5406653210f24dee9722b975c
Dipesh Patel <dipthegeezer@opensource.gmail.com>
2012-07-27 17:09:43 +0100

    Sorted out REST api now just need to build it

--- src/erldslabb_user.erl
+++ src/erldslabb_user.erl
@@ -43,8 +43,18 @@ init([Hostname,Port,Database,Username,Password]) ->
                   ),
     {ok, #state{conn=Conn}}.
 
-handle_call({squery, Sql}, _From, #state{conn=Conn}=State) ->
-    {reply, pgsql:squery(Conn, Sql), State};
+%handle_call({squery, Sql}, _From, #state{conn=Conn}=State) ->
+%    {reply, pgsql:squery(Conn, Sql), State};
+%handle_call({equery, Stmt, Params}, _From, #state{conn=Conn}=State) ->
+%    {reply, pgsql:equery(Conn, Stmt, Params), State};
+handle_call({add_user, Params}, _From, #state{conn=Conn}=State) ->
+    {reply,
+     pgsql:equery(
+              Conn,
+              "INSERT INTO users (email, username, password, salt, date_of_birth)"
+              ++ "VALUES($1, $2, $3, $4, $5)", Params
+             ),
+     State};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
@@ -66,3 +76,21 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
+get_timestamp() ->
+    {Megaseconds,Seconds,Microseconds} = erlang:now(),
+    (Megaseconds*1000000+Seconds)*1000000+Microseconds.
+
+hash_password(Salt, Password) ->
+    Cxt = crypto:hmac_init(sha,Salt),
+    Cxt2 = crypto:hmac_update(Cxt,Password),
+    Mac = crypto:hmac_final(Cxt2),
+    hexstring(Mac).
+
+hexstring(<<X:128/big-unsigned-integer>>) ->
+    lists:flatten(io_lib:format("~32.16.0b", [X]));
+hexstring(<<X:160/big-unsigned-integer>>) ->
+    lists:flatten(io_lib:format("~40.16.0b", [X]));
+hexstring(<<X:256/big-unsigned-integer>>) ->
+    lists:flatten(io_lib:format("~64.16.0b", [X]));
+hexstring(<<X:512/big-unsigned-integer>>) ->
+    lists:flatten(io_lib:format("~128.16.0b", [X])).
--- src/erldslabb_user_resource.erl
+++ src/erldslabb_user_resource.erl
@@ -3,7 +3,19 @@
 %% @doc Resource for User
 
 -module(erldslabb_user_resource).
--export([init/1, to_json/2, content_types_provided/2, allowed_methods/2]).
+-export([
+         init/1,
+         content_types_accepted/2,
+         content_types_provided/2,
+         allowed_methods/2,
+         resource_exists/2,
+         allow_missing_post/2,
+         process_post/2,
+         delete_resource/2,
+         finish_request/2,
+         to_json/2,
+         from_json/2
+        ]).
 -include_lib("webmachine/include/webmachine.hrl").
 
 init([]) -> {ok, undefined}.
@@ -11,37 +23,49 @@ init([]) -> {ok, undefined}.
 content_types_provided(ReqData, Context) ->
     {[{"application/json", to_json}], ReqData, Context}.
 
+content_types_accepted(ReqData, Context) ->
+    {[{"application/json", from_json}], ReqData, Context}.
+
 allowed_methods(ReqData, Context) ->
-    {['GET','POST','DELETE'], ReqData, Context}.
+    {['GET','POST','DELETE','HEAD','PUT'], ReqData, Context}.
 
-to_json(ReqData, State) ->
-    Args = wrq:path_tokens(ReqData),
-    Method = wrq:method(ReqData),
-    io:fwrite("Args ~p Params ~p ~n",[Args, Method]),
-    Result = execute_action(Args, ReqData),
-    {Result, ReqData, State}.
+finish_request(ReqData, Context) ->
+    {true, ReqData, Context}.
 
-execute_get(["find"], _) ->
-    io:fwrite("Args ~p Params ~n",["find"]),
-    "{moo:bar}";
-execute_get([Id], _) ->
-    io:fwrite("Args ~p Params ~n",[Id]),
-    "{id:Id}".
+allow_missing_post(ReqData, Context) ->
+    {true, ReqData, Context}.
 
-process_post(ReqData, State) ->
-    Args = wrq:path_tokens(ReqData),
-    Method = wrq:method(ReqData),
-    io:fwrite("Args ~p Params ~p ~n",[Args, Method]),
-    Result = execute_post(Args, ReqData),
-    {Result, ReqData, State}.
+process_post(ReqData, Context) ->
+    [{JsonDoc, _}] = mochiweb_util:parse_qs(wrq:req_body(ReqData)),
+    {struct, Doc} = mochijson2:decode(JsonDoc),
+    io:fwrite("Args ~p post ~n",[Doc]),
+    {true, ReqData, Context}.
 
-execute_post(["create"], _) ->
-    io:fwrite("Args ~p Params ~n",["create"]),
-    "{foo:bar}";
+delete_resource(ReqData, Context) ->
+    Id = wrq:path_info(id, ReqData),
+    io:fwrite("Delete ~p ~n",[Id]),
+    {true, ReqData, Context}.
+    %case prp_schema:delete_paper(Id) of
+     %   ok    -> {true, RD, Ctx};
+     %   _Else -> {false, RD, Ctx}
+    %end.
 
+resource_exists(ReqData, Context) ->
+    Id = wrq:path_info(id, ReqData),
+    io:fwrite("Exists ~p ~n",[Id]),
+    {true, ReqData, Context}.
 
-delete_resource(ReqData, State) ->
-    Args = wrq:path_tokens(ReqData),
-    io:fwrite("Args ~p Params ~n",[Args]),
-    {true, ReqData ,State}.
+to_json(ReqData, Context) ->
+    Id = wrq:path_info(id, ReqData),
+    % to fill in get etc.
+    Resp = "{id:" ++ Id ++ "}",
+    {Resp, ReqData, Context}.
 
+from_json(ReqData, Context) ->
+    Id = wrq:path_info(id, ReqData),
+    [{JsonDoc, _}] = mochiweb_util:parse_qs(wrq:req_body(ReqData)),
+    {struct, Doc} = mochijson2:decode(JsonDoc),
+    io:fwrite("Args ~p post ~n",[Doc]),
+    Resp2 = wrq:set_resp_body("{put_id:" ++ Id ++ "}", ReqData),
+    io:fwrite("Putting ~p ~n",[Id]),
+    {true, Resp2, Context}.
