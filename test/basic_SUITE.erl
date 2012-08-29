1b30708d10210a183259e950d08406890d44562c
Dipesh Patel <dipthegeezer.opensource@gmail.com>
2012-08-29 13:29:05 +0100

    test changes

--- test/basic_SUITE.erl
+++ test/basic_SUITE.erl
@@ -4,10 +4,15 @@
 -export([all/0]).
 -export([run_erldslabb_util/1,
          run_erldslabb_pg_server/1,
-         run_erldslabb_session_server/1
+         run_erldslabb_session_server/1,
+         init_per_suite/1
         ]).
 
 all() -> [run_erldslabb_util, run_erldslabb_pg_server, run_erldslabb_session_server].
+
+init_per_suite(InitConfigData) ->
+    io:fwrite(standard_error,"config ~p~n",[InitConfigData]),
+    InitConfigData.
 
 run_erldslabb_util(_Config) ->
     ok = eunit:test(erldslabb_util).
